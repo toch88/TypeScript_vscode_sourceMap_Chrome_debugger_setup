7028a0d6aad5fe5644bcef0ed32902c638fcd48c
Mateusz <tocha.mateusz@gmail.com>
2018-05-30 09:27:23 +0200

    Init commit

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log("Hello ");
+        return 0;
+    };
+    return Startup;
+}());
+var zmienna = 5;
+Startup.main();
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(): number {
+
+        console.log("Hello ");
+        return 0;
+    }
+}
+let zmienna = 5;
+Startup.main();
